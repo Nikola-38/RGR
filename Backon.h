1c579869d19a7395fa146c05d59f06551ca347b8
Nikola-38 <163429541+Nikola-38@users.noreply.github.com>
2024-06-16 20:36:59 +0700

    Create Backon.h

---
+++ Backon.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string encryptBaconian(const string& plaintext);
+string decryptBaconian(const string& ciphertext);
+void Bacon(string& password1, string& password2);
