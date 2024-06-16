4f3f22069c1f7d35fd3b01b3691db726e2b683e7
Nikola-38 <163429541+Nikola-38@users.noreply.github.com>
2024-06-16 20:42:37 +0700

    Update SenSira.cpp

--- SenSira.cpp
+++ SenSira.cpp
@@ -1,3 +1,4 @@
+#include "Read.h"
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -6,30 +7,6 @@
 
 using namespace std;
 
-void writeToFile(const string& filename, const string& text) {
-    ofstream file(filename);
-    if (file.is_open()) {
-        file << text;
-        file.close();
-    }
-    else {
-        throw logic_error("Невозможно записать данные в файл: " + filename);
-    }
-}
-
-string readFromFile(const string& filename) {
-    ifstream file(filename);
-    if (!file.is_open()) {
-        throw logic_error("Невозможно прочитать файл: " + filename);
-    }
-
-    string content, line;
-    while (getline(file, line)) {
-        content += line + "\n";
-    }
-    file.close();
-    return content;
-}
 
 string encrypt_SenSira(const string& plaintext, const string& key) {
     string ciphertext;
@@ -65,80 +42,97 @@ void SenSira(const string& password1, const string& password2) {
 
     while (true) {
         try {
-            cout << "------SenSira------" << endl
-                << "Select an action:" << endl
-                << "<1> Encrypt" << endl
-                << "<2> Decrypt" << endl
-                << "<3> Exit to the main menu" << endl;
+            
+            cout << "_______SenSira_______" << endl;
+            cout << "Выберите действие:" << endl;
+            cout << "<1> Зашифровать" << endl;
+            cout << "<2> Дешифровать" << endl;
+            cout << "<3> Выход" << endl;
+            cout << "Введите свой выбор:" << endl;
+            cout << "______________________" << endl;
+
             cin >> choice;
             if (cin.fail() || cin.peek() != '\n') {
-                throw logic_error("Input error!");
+                throw logic_error("Ошибка ввода!");
             }
+
+            cin.ignore(); // Clear the input buffer
             
+            // Request the key only once
+            cout << "Введите ключ шифрования: ";
+            getline(cin, key);
 
             switch (choice) {
             case 1: {
-                cout << "Enter the encoding password: ";
+                cout << "Введите пароль для кодировки: ";
                 getline(cin, pass1);
                 if (pass1 != password1) {
-                    throw logic_error("Invalid encoding password");
+                    throw logic_error("Неверный пароль для кодировки");
                 }
-                cout << "Input action:" << endl
-                    << "<1> Enter text from the console" << endl
-                    << "<2> Read text from a file" << endl;
+                cout << "Входное действие:" << endl
+                    << "<1> Введите текст с консоли" << endl
+                    << "<2> Чтение текста из файла" << endl;
                 cin >> encryptionChoice;
                 if (cin.fail() || cin.peek() != '\n') {
-                    throw logic_error("Input error!");
+                    throw logic_error("Ошибка ввода!");
                 }
-                
+
+                cin.ignore(); // Clear the input buffer
+
                 if (encryptionChoice == 1) {
-                    cout << "Enter the text to encrypt: ";
+                    cout << "Введите текст для шифрования: ";
+                    string message;
                     getline(cin, message);
-                    cout << "Enter the encryption key: ";
-                    getline(cin, key);
+                    string filename;
+                    cout << "Введите имя файла для сохранения введенного текста: ";
+                    getline(cin, filename);
+                    writeToFile(filename, message);
                     string ciphertext = encrypt_SenSira(message, key);
                     writeToFile("encrypted_SenSira.txt", ciphertext);
-                    cout << "Encrypted text written to file: encrypted_SenSira.txt" << endl;
+                    cout << "Введенный текст сохранен в файл: " << filename << endl;
+                    cout << "Зашифрованный текст, записанный в файл: encrypted_SenSira.txt" << endl;
                 }
+                
                 else if (encryptionChoice == 2) {
-                    cout << "Enter the name of the file with the extension: ";
+                    cout << "Введите имя файла с расширением: ";
                     getline(cin, filename);
                     string plaintext = readFromFile(filename);
-                    cout << "Enter the encryption key: ";
-                    getline(cin, key);
                     string ciphertext = encrypt_SenSira(plaintext, key);
                     writeToFile("encrypted_SenSira.txt", ciphertext);
-                    cout << "Encrypted text written to file: encrypted_SenSira.txt" << endl;
+                    cout << "Зашифрованный текст, записанный в файл: encrypted_SenSira.txt" << endl;
+                   
                 }
+           
                 else {
-                    throw logic_error("Invalid choice");
+                    throw logic_error("Неверный выбор");
                 }
                 break;
+                
             }
             case 2: {
-                cout << "Enter the decoding password: ";
+                system("cls");
+                cout << "Введите пароль для расшифровки: ";
                 getline(cin, pass2);
                 if (pass2 != password2) {
-                    throw logic_error("Invalid decoding password");
+                    throw logic_error("Неверный пароль для расшифровки");
                 }
                 string ciphertext = readFromFile("encrypted_SenSira.txt");
-                cout << "Enter the decryption key: ";
-                getline(cin, key);
                 string plaintext = decrypt_SenSira(ciphertext, key);
                 writeToFile("decrypted_SenSira.txt", plaintext);
-                cout << "Decrypted text written to file: decrypted_SenSira.txt" << endl;
+                cout << "Расшифрованный текст, записанный в файл: decrypted_SenSira.txt" << endl;
                 break;
+                
             }
             case 3:
                 return;
             default:
-                throw logic_error("Invalid choice");
+                throw logic_error("Неверный выбор");
             }
         }
         catch (const exception& e) {
             cerr << e.what() << endl;
             cin.clear(); // Clear the error state
-            
+
             system("pause");
         }
     }
