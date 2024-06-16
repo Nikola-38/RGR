762d9e4dd504daf898d77f83f3e0d0c5ff8be26a
Nikola-38 <163429541+Nikola-38@users.noreply.github.com>
2024-06-16 20:43:55 +0700

    Update Backon.cpp

--- Backon.cpp
+++ Backon.cpp
@@ -1,38 +1,10 @@
+#include "Read.h"
 #include <iostream>
 #include <string>
 #include <fstream>
 
 using namespace std;
 
-void writeToFile(const string& filename, const string& text) {
-    ofstream file(filename);
-    if (file.is_open()) {
-        file << text;
-        file.close();
-    }
-    else {
-        throw logic_error("Невозможно записать данные в файл: " + filename);
-    }
-}
-
-string readFromFile(const string& filename) {
-    ifstream file(filename);
-    if (!file.is_open()) {
-        throw logic_error("Невозможно прочитать файл: " + filename);
-    }
-
-    string content, line;
-    while (getline(file, line)) {
-        content += line + "\n";
-    }
-    file.close();
-    return content;
-}
-
-bool exist(const string& filename) {
-    ifstream file(filename);
-    return file.good();
-}
 
 string encryptBaconian(const string& plaintext) {
     string ciphertext;
@@ -49,12 +21,12 @@ string decryptBaconian(const string& ciphertext) {
     try {
         for (size_t i = 0; i < ciphertext.length(); i += 8) {
             if (i + 7 >= ciphertext.length()) {
-                throw out_of_range("Ciphertext is not a multiple of 8 characters");
+                throw out_of_range("Зашифрованный текст не должен быть кратен 8 символам");
             }
             char ch = 0;
             for (int j = 0; j < 8; ++j) {
                 if (ciphertext[i + j] != 'a' && ciphertext[i + j] != 'b') {
-                    throw invalid_argument("Ciphertext contains invalid characters");
+                    throw invalid_argument("Зашифрованный текст содержит недопустимые символы");
                 }
                 ch = (ch << 1) | (ciphertext[i + j] == 'b');
             }
@@ -73,93 +45,105 @@ void Bacon(string& password1, string& password2) {
     string pass1, pass2, message, filename, encryptedFilename, decryptedFilename;
 
     while (true) {
-#ifdef _WIN32
-        system("cls");
-#else
-        system("clear");
-#endif
-        cout << "------Bacon Cipher-----" << endl;
-        cout << "Select an action:" << endl;
-        cout << "<1> Encrypt" << endl;
-        cout << "<2> Decrypt" << endl;
-        cout << "<3> Exit" << endl;
-        cout << "Enter your choice: ";
+       
+        cout << "______Bacon Cipher_____" << endl;
+        cout << "Выберите действие:" << endl;
+        cout << "<1> Зашифровать" << endl;
+        cout << "<2> Дешифровать" << endl;
+        cout << "<3> Выход" << endl;
+        cout << "Введите свой выбор:" << endl;
+        cout << "______________________" << endl;
         cin >> pick;
 
         try {
             if (pick != 1 && pick != 2 && pick != 3) {
-                throw logic_error("You entered the wrong command!");
+                throw logic_error("Вы ввели неправильную команду!");
             }
 
             if (pick == 1) {
-                cout << "Enter the encoding password: ";
+                system("cls");
+                cout << "Введите пароль для кодировки: ";
                 cin >> pass1;
+                system("cls");
                 if (pass1 == password1) {
-                    cout << "Input action:" << endl
-                        << "<1> Enter text from the console" << endl
-                        << "<2> Read text from a file" << endl;
+                    cout << "Входное действие:" << endl
+                        << "<1> Введите текст с консоли" << endl
+                        << "<2> Чтение текста из файла" << endl;
                     cin >> pickencode;
                     if (cin.fail() || cin.peek() != '\n') {
-                        throw logic_error("Input error!");
+                        throw logic_error("Ошибка ввода!");
                     }
                     if (pickencode != 1 && pickencode != 2) {
-                        throw logic_error("You entered the wrong command!");
+                        throw logic_error("Вы ввели неправильную команду!");
                     }
                     if (pickencode == 1) {
-                        cout << "Input text: ";
-                        cin.ignore();
+                        cout << "Ввод текста: ";
+                        cin.ignore(); // Очистка буфера ввода
                         getline(cin, message);
 
-                        string ciphertext = encryptBaconian(message);
-                        cout << "Encrypted message: " << ciphertext << endl;
-                        encryptedFilename = "encrypted_baconian.txt";
-                        writeToFile(encryptedFilename, ciphertext); // Запись зашифрованного текста в файл
-                        cout << "Encrypted text saved to: " << encryptedFilename << endl;
-                    }
-                    else if (pickencode == 2) {
-                        cout << "Enter the name of the file with the extension: ";
-                        cin >> filename;
+                        string originalFilename = "Backon_original_input.txt";
+                        writeToFile(originalFilename, message); // Запись введенного текста в файл
+                        cout << "Введенный текст сохранен в файл: " << originalFilename << endl;
 
+                        string ciphertext = encryptBaconian(message);
+                        string encryptedFilename = "encrypted_baconian.txt";
+                        writeToFile(encryptedFilename, ciphertext); // Запись зашифрованного текста в файл
+                        cout << "Зашифрованный текст, сохраненный в: " << encryptedFilename << endl;
+                        
+                        
+                    }
+
+                    else if (pickencode == 2) {
+                        system("cls");
+                        
+                        cout << "Введите имя файла с расширением:";
+                        cin >> filename;
+                       
                         if (!exist(filename)) {
-                            throw logic_error("File does not exist or cannot be opened!");
+                            throw logic_error("Файл не существует или не может быть открыт!");
                         }
 
                         string plaintext = readFromFile(filename);
                         string ciphertext = encryptBaconian(plaintext);
-                        cout << "Encrypted message: " << ciphertext << endl;
+                       
                         encryptedFilename = "encrypted_baconian.txt";
                         writeToFile(encryptedFilename, ciphertext); // Запись зашифрованного текста в файл
-                        cout << "Encrypted text saved to: " << encryptedFilename << endl;
+                        cout << "Зашифрованный текст, сохраненный в: " << encryptedFilename << endl;
+                        
+                        
                     }
                 }
                 else {
-                    throw logic_error("Invalid encoding password");
+                    throw logic_error("Неверный пароль для кодировки");
                 }
             }
+            
             else if (pick == 2) {
-                cout << "Enter the decoding password: ";
+                system("cls");
+                cout << "Введите пароль для расшифровки: ";
                 cin >> pass2;
-
+                
                 if (pass2 == password2) {
-                    cout << "Enter the name of the file with the encrypted message: ";
+                    cout << "Введите название файла с зашифрованным сообщением: ";
                     cin >> filename;
 
                     if (!exist(filename)) {
-                        throw logic_error("File does not exist or cannot be opened!");
+                        throw logic_error("Файл не существует или не может быть открыт!");
                     }
 
                     string ciphertext = readFromFile(filename);
                     string plaintext = decryptBaconian(ciphertext);
-                    cout << "Decrypted message: " << plaintext << endl;
+                    
                     decryptedFilename = "decrypted_baconian.txt";
                     writeToFile(decryptedFilename, plaintext); // Запись дешифрованного текста в файл
-                    cout << "Decrypted text saved to: " << decryptedFilename << endl;
+                    cout << "Расшифрованный текст, сохраненный в: " << decryptedFilename << endl;
                 }
                 else {
                     throw logic_error("Invalid decoding password");
                 }
             }
             else if (pick == 3) {
+                system("cls");
                 return;
             }
         }
