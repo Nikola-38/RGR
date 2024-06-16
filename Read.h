448a8c25fed09ce4cf3695a7fbc953fb769b9e52
Nikola-38 <163429541+Nikola-38@users.noreply.github.com>
2024-06-16 20:39:45 +0700

    Create Read.h

---
+++ Read.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+void writeToFile(const string& filename, const string& text);
+string readFromFile(const string& filename);
+bool exist(const string& filename);
